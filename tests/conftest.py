91d5f3931ef8c242c3560ae52586d44ce3d17754
zhandos <zhandosshakimurat@gmail.com>
2024-09-06 01:07:47 +0500

    redis tests update

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import pytest_asyncio
 
 @pytest_asyncio.fixture
 async def client():
-    async with AsyncClient(ASGITransport(app=app), base_url="http://test") as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
