d65c407391fa98c450d97a73eaa1aa3b5eace15a
zhandos <zhandosshakimurat@gmail.com>
2024-09-13 00:49:10 +0500

    celery

--- src/drugs/router.py
+++ src/drugs/router.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, HTTPException, status
 from src.database import async_session_maker
 from sqlalchemy.future import select
 from sqlalchemy import delete
-from rdkit import Chem
+from src.tasks import add_task_substructure_search
 from src.config import logger
-from src.utils import set_cache, get_cached_result
-from . import models, schemas
+from celery.result import AsyncResult
+from src.celery_worker import celery
+from src.drugs import models, schemas
 
 
 router = APIRouter(prefix="/drugs", tags=["drugs"])
@@ -25,28 +26,20 @@ async def add_drug(drug_data: schemas.DrugCreate):
 
 
 @router.get("/search")
-async def substructure_search(mol: str):
-    cache_key = f"search:{mol}"
-    cached_result = get_cached_result(cache_key)
-    if cached_result is not None:
-        logger.info("Return cached result")
-        return cached_result
-    
-    async with async_session_maker() as session:
-        mol = Chem.MolFromSmiles(mol)
-        query = select(models.Drug)
-        query_result = await session.execute(query)
-        drugs = query_result.scalars().all()
-        if not mol:
-            logger.warning("Invalid SMILE")
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail='Invalid input')
-        result = [drug for drug in drugs if Chem.MolFromSmiles(
-            drug.smiles).HasSubstructMatch(mol)]
-        set_cache(cache_key, result)
-        logger.info(f"Set cache - {cache_key}")
-        logger.info("Performed substructure search")
-        return result
+async def start_substructure_search(mol: str):
+    task = add_task_substructure_search.delay(mol)
+    return {"task_id": task.id, "status": task.status}
+
+
+@router.get("/search/{task_id}")
+async def get_substructure_search_result(task_id: str):
+    task_result = AsyncResult(task_id, app=celery)
+    if task_result.state == 'PENDING':
+        return {"task_id": task_id, "status": "Task is still processing"}
+    elif task_result.state == 'SUCCESS':
+        return {"task_id": task_id, "status": "Task completed", "result": task_result.result}
+    else:
+        return {"task_id": task_id, "status": task_result.state}
 
 
 @router.get("/{drug_id}")
